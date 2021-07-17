706a784a5f088ad18e3e294dc1cdfa744a1a6341
Pouapheng Yang <phewy06@yahoo.com>
2021-07-16 21:57:57 -0500

    added feedback router

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-const feedbackRouter = require('./server/server.js');
+const feedbackRouter = require('./routes/feedback.router.js');
 app.use('/api/feedback', feedbackRouter);
 
 /** ---------- START SERVER ---------- **/
---
+++ src/components/FeelingsPage/FeelingsPage.jsx
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+
+
+
+function FeelingsPage() {
+
+    const dispatch = useDispatch();
+    
+    // axios GET all feelings
+function getfeelings() {
+    axios({
+        method: 'GET',
+        url: '/api/'
+    })
+    .then((response) => {
+        console.log(response.data);
+        dispatch({ type: 'GET_FEELINGS', payload: response.data,});
+    })
+    .catch((error) => {
+        console.log('GET FEELINGS ERROR', error);
+    });
+}
+ 
+    
+    return (
+        <div classname="App">
+            <p>How are you feelings today?</p>
+            <table>
+                <th>
+                    <tr>
+                        <th>Input</th>
+                    </tr>
+
+                </th>
+                {/* <StufftoADD/> */}
+            </table>
+        </div>
+
+    );
+}
+
+export default FeelingsPage;
